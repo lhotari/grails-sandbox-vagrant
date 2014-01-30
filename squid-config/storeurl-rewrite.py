420f2bf8bd9ceea686303fb15178675988692db4
Lari Hotari <lhotari@gopivotal.com>
2014-01-30 19:05:06 +0200

    remove logging

--- squid-config/storeurl-rewrite.py
+++ squid-config/storeurl-rewrite.py
@@ -2,19 +2,11 @@
 import sys
 import re
 
-log = open('/usr/local/var/squid/logs/storeurl-rewrite.log', 'w')
-
 while True:
 	line = sys.stdin.readline()
-	log.write(line)
 	if not line: break	
 	channel, url, other = line.split(" ", 2)
 	if 'download.oracle.com' in url:
 		url = re.sub(r'(.+)\?AuthParam=(.+)$', r'\1', url)
 	print channel, url
-	log.write(url)
-	log.write('\n')
-	log.flush()
 	sys.stdout.flush()
-
-log.close()
