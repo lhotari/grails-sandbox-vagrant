ed81077653bcef441247bcf40d0ed1ba43f94358
Lari Hotari <lhotari@pivotal.io>
2014-08-26 13:20:18 +0300

    add MacOSX support for mount_grails_home.sh script

--- mount_grails_home.sh
+++ mount_grails_home.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-# mount $GRAILS_HOME to sub-directory in Linux
-sudo mount -o bind $GRAILS_HOME grails-home-mount
+if [ "`uname`"=="Darwin" ]; then
+   # brew install bindfs
+   bindfs $GRAILS_HOME grails-home-mount
+else
+   # mount $GRAILS_HOME to sub-directory in Linux
+   sudo mount -o bind $GRAILS_HOME grails-home-mount
+fi
--- umount_grails_home.sh
+++ umount_grails_home.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-sudo umount grails-home-mount
+if [ "`uname`"=="Darwin" ]; then
+   umount grails-home-mount
+else
+   sudo umount grails-home-mount
+fi
